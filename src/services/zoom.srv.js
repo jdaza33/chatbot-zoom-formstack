d28fccdb0b2430a9b124123f71f50009b138ec7f
J. Daza <blackencio33@gmail.com>
2021-03-23 18:57:56 -0400

    [hack] changes

--- src/services/zoom.srv.js
+++ src/services/zoom.srv.js
@@ -351,10 +351,10 @@ const startBot = (userId) => {
               type: 'select',
               text: 'Formularios',
               select_items: forms.map((f) => {
-                return {
+                return JSON.stringify({
                   text: f.name,
                   value: `select_form_sesion|${f.id}`,
-                }
+                })
               }),
             },
           ],
